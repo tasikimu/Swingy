143db0ed528c248181befa682a955078a3b62edd
Tjelaje <30749021+tasikimu@users.noreply.github.com>
2019-08-14 08:15:49 +0200

    Update Artifacts.java

--- src/com/asikimu/swingy/model/Weapons/Artifacts.java
+++ src/com/asikimu/swingy/model/Weapons/Artifacts.java
@@ -1,4 +1,25 @@
 package com.asikimu.swingy.model.Weapons;
 
 public class Artifacts {
+  private String type;
+    private static final String artifacts[] = {"WEAPON", "ARMOR", "HELM"};
+
+    public static String randomiseArtifact()
+    {
+		Random random = new Random();
+		return (artifacts[random.nextInt(3)]);
+	}
+
+	Artifacts(String type){
+		this.type = type;
+	}
+
+	public static String[] getArtifacts() {
+		return artifacts;
+	}
+
+	public String getType(){
+		return this.type;
+	}
+}
 }
