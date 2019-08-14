bf8023b4d07d3226c7298ecea365ddf2702eb8be
Tjelaje <30749021+tasikimu@users.noreply.github.com>
2019-08-14 08:11:13 +0200

    Update Hero.java

--- src/com/asikimu/swingy/model/CreateHero/Hero.java
+++ src/com/asikimu/swingy/model/CreateHero/Hero.java
@@ -1,4 +1,36 @@
 package com.asikimu.swingy.CreateHero;
 
 public class Hero {
+  @NotNull
+        private String newHero;
+        @NotNull
+        private HeroStats stats = new HeroStats();
+        @NotNull
+        private Artifacts artifact;
+
+        public Hero(){
+
+        }
+    
+        protected Hero (String newHero, HeroStats stats, Artifacts artifact){
+            this.newHero = newHero;
+            this.stats = stats;
+            this.artifact = artifact;
+        }
+
+        public HeroStats getHeroStats() {
+            return stats;
+        }
+
+        public Artifacts getArtifact(){
+            return artifact;
+        }
+
+        public String getnewHero(){
+            return this.newHero;
+        }
+
+        public void setArtifact(Artifacts artifact){
+            this.artifact = artifact;
+        }
 }
