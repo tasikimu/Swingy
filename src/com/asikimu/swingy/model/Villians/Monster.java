afecab18fe8e11e36fa3ea5d88575eff4f7a30aa
Tjelaje <30749021+tasikimu@users.noreply.github.com>
2019-08-14 08:13:06 +0200

    Update Monster.java

--- src/com/asikimu/swingy/model/Villians/Monster.java
+++ src/com/asikimu/swingy/model/Villians/Monster.java
@@ -1,4 +1,10 @@
 package com.asikimu.swingy.Villians;
 
-public class Monster {
+public class Monster  extends Villian {
+
+	public Monster(int level, int attack, int defense, int hitpoints, int experience, Artifacts artifact){
+		super(level, attack, defense, hitpoints, experience, artifact);
+		int typeID = 2;
+		super.setTypeID(typeID);
+	}
 }
