28b959737b95869c29c88548f283d863a231c530
Tjelaje <30749021+tasikimu@users.noreply.github.com>
2019-08-14 08:07:42 +0200

    Update Controller.java

--- src/com/asikimu/swingy/map/Controller.java
+++ src/com/asikimu/swingy/map/Controller.java
@@ -1,4 +1,103 @@
 package com.asikimu.swingy.map;
 
 public class Controller {
+   public Controller() {
+    }
+
+    public static void run(Hero hero) {
+        ConsoleMap printMap = new ConsoleMap(hero);
+
+        printMap.printMap();
+        HeroDisplay.printDirections();
+        Console console = System.console();
+        while (true) {
+            String line = console.readLine();
+
+
+            if (line.matches("\\s*[1-5]\\s*")) {
+                int direction = Integer.parseInt(line);
+
+                if (direction == 1) {
+                    printMap.updateHeroPosition(1, 0);
+                    printMap.printMap();
+                    HeroDisplay.printDirections();
+                } else if (direction == 2) {
+                    printMap.updateHeroPosition(0, 1);
+                    printMap.printMap();
+                    HeroDisplay.printDirections();
+                } else if (direction == 3) {
+                    printMap.updateHeroPosition(-1, 0);
+                    printMap.printMap();
+                    HeroDisplay.printDirections();
+                }
+                else if (direction == 4) {
+                    printMap.updateHeroPosition(0, -1);
+                    printMap.printMap();
+                    HeroDisplay.printDirections();
+                }
+                    else if(direction == 5)
+                    {
+                        System.exit(0);
+                    }
+            }
+            else {
+                System.out.println("invalid input");
+
+            }
+        }
+    }
+
+    public static boolean inLuck() {
+        Random random = new Random();
+        int luck = random.nextInt(2) + 1;
+
+        if (luck == 1)
+            return true;
+        return false;
+    }
+
+    public static int fight(Hero hero, Villian enemy) {
+        int fight = 0, won = 0, hit = 0;
+        Random random = new Random();
+
+        if (inLuck() == true || hero.getHeroStats().getPower() > enemy.getPower()) {
+            fight = 1;
+        }
+
+        if (hero.getHeroStats().getHitPoints() > 0) {
+            while (hero.getHeroStats().getHitPoints() > 0 && enemy.getHitPoints() > 0) {
+                System.out.println("Hero hp: " + hero.getHeroStats().getHitPoints());
+                System.out.println("Enemy hp: " + enemy.getHitPoints());
+                if (fight == 0) {
+                    hit = random.nextInt(30) + 1;
+                    if (enemy.getHitPoints() > 0) {
+                        hero.getHeroStats().setHitPoints(-hit);
+                        ReadFile.updateFile(hero);
+                        System.out.println("The Enemy attacked you and you lost " + hit + " hitpoints.");
+
+                        if (hero.getHeroStats().getHitPoints() <= 0) {
+                            won = 0;
+                            break;
+                        }
+                        fight = 1;
+                    }
+                } else if (fight == 1) {
+                    hit = random.nextInt(50) + 1;
+                    if (hero.getHeroStats().getHitPoints() > 0) {
+                        enemy.setHitPoints(-hit);
+                        System.out.println("You attacked the Villian and gave " + hit + " damage.");
+                        if (enemy.getHitPoints() <= 0) {
+                            won = 1;
+                            break;
+                        }
+                        fight = 0;
+                    }
+                }
+            }
+        }
+        else
+            System.out.println("You are too weak to fight, go regain your strength!!!\n\n" +
+                    "Your HP is " + hero.getHeroStats().getHitPoints());
+        return won;
+    }
 }
