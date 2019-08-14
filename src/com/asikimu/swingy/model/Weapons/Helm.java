32ba230684ded4581577d3b520e038dfa09a2438
Tjelaje <30749021+tasikimu@users.noreply.github.com>
2019-08-14 08:16:23 +0200

    Update Helm.java

--- src/com/asikimu/swingy/model/Weapons/Helm.java
+++ src/com/asikimu/swingy/model/Weapons/Helm.java
@@ -1,4 +1,13 @@
 package com.asikimu.swingy.model.Weapons;
 
-public class Helm {
+public class Helm extends Artifacts {
+	private int hitpoints = 85;
+
+	public Helm(String type){
+		super(type);
+	}
+
+	public int getHitPoints(){
+		return this.hitpoints;
+	}
 }
