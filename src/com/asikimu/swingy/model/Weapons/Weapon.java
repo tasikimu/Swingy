54e9b3669a878d8c7d9fff328d8d1e17fb85c926
Tjelaje <30749021+tasikimu@users.noreply.github.com>
2019-08-14 08:17:02 +0200

    Update Weapon.java

--- src/com/asikimu/swingy/model/Weapons/Weapon.java
+++ src/com/asikimu/swingy/model/Weapons/Weapon.java
@@ -1,4 +1,13 @@
 package com.asikimu.swingy.model.Weapons;
 
-public class Weapon {
+public class Weapon extends Artifacts{
+	private int attack = 75;
+
+	public Weapon(String type){
+		super(type);
+	}
+
+	public int getAttack(){
+		return this.attack;
+	}
 }
