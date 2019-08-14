8dded9d4e45dc4e4f9b785e9ed02e9b7d401c7da
Tjelaje <30749021+tasikimu@users.noreply.github.com>
2019-08-14 08:11:51 +0200

    Update HeroStats.java

--- src/com/asikimu/swingy/model/CreateHero/HeroStats.java
+++ src/com/asikimu/swingy/model/CreateHero/HeroStats.java
@@ -1,4 +1,84 @@
 package com.asikimu.swingy.CreateHero;
 
 public class HeroStats {
+  private String heroType;
+	private int level;
+	private int attack;
+	private int defense;
+	private int hitpoints;
+	private int experience;
+	private int power;
+
+	public HeroStats(){
+
+	}
+
+	public HeroStats(String type, int level, int attack, int defense, int hitpoints, int experience){
+		this.heroType = type;
+		this.level = level;
+		this.attack = attack;
+		this.defense = defense;
+		this.hitpoints = hitpoints;
+		this.experience = experience;
+		this.power = attack + defense;
+	}
+
+	//GETTERS
+
+	public String getHeroType(){
+		return this.heroType;
+	}
+
+	public int getLevel(){
+		return this.level;
+	}
+
+	public int getAttack(){
+		return this.attack;
+	}
+
+	public int getDefense(){
+		return this.defense;
+	}
+
+	public int getHitPoints(){
+		return this.hitpoints;
+	}
+
+	public int getExperience(){
+		return this.experience;
+	}
+
+	public int getPower(){
+		return this.power;
+	}
+
+
+	// SETTERS
+
+	public void setLevel(int level){
+		this.level = level;
+	}
+
+	public void setAttack(int attack){
+		this.attack = attack;
+	}
+
+	public void setDefense(int defense){
+		this.defense += defense;
+		if (this.defense <= 0)
+			this.defense = 0;
+	}
+	
+	public void setHitPoints(int hitpoints){
+		this.hitpoints += hitpoints;
+		if (this.hitpoints <= 0)
+			this.hitpoints = 0;
+	}
+
+	public void setExperience(int experience){
+        this.experience = experience;
+	}
+
+}
 }
